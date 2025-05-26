6f979553b67203e18796fb98f1e03f8d3450ccf9
DevendraAmbati <122284843+DevendraAmbati@users.noreply.github.com>
2025-05-26 10:59:48 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Suspense, lazy } from 'react'
 import Header from './components/Header'
-import Blog from './pages/Blog'
-import BlogDetails from './pages/BlogDetails'
 import Footer from './components/Footer'
+
+const Blog = lazy(() => import('./pages/Blog'))
+const BlogDetails = lazy(() => import('./pages/BlogDetails'))
 
 function App() {
   return (
     <Router>
       <Header/>
+      <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
       <Routes>
         <Route path="/" element={<Blog />} />
         <Route path="/blog/:id" element={<BlogDetails />} />
       </Routes>
+        </Suspense>
       <Footer/>
     </Router>
   )
